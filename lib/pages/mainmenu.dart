90dc6520ac41ed29fe0b84f28f2c5c7505cb964e
pc <al23760375@ite.edu.mx>
2026-04-02 22:00:22 -0700

    - Cambios  * Agregadas distintas paginas para cada caso de uso  * Alguien lee esto?

---
+++ lib/pages/mainmenu.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Mainmenu extends StatelessWidget {
+  const Mainmenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Menu principal', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
---
+++ lib/pages/searcher.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Searcher extends StatelessWidget {
+  const Searcher({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Buscador', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
