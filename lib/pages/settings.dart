14484c67db838ff27bf7480b35c68ccea982c0fe
pc <al23760375@ite.edu.mx>
2026-04-23 14:33:04 -0700

    - Agregadas las notificaciones

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -3,13 +3,20 @@ import 'package:nutriis/notifications.dart';
 class FontSizeController {
   static ValueNotifier<double> scale = ValueNotifier(1.0);
 }
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({super.key});
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  double selectedScale = 1.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Configuración")),
+      appBar: AppBar(title: const Text("Configuración"), backgroundColor: Colors.white),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
@@ -21,34 +28,31 @@ class Settings extends StatelessWidget {
 
             const SizedBox(height: 20),
 
-            ElevatedButton(
-              onPressed: () {
-                NotiService.mostrarNotificacion();
+            DropdownButton<double>(
+              value: selectedScale,
+              isExpanded: true,
+              items: const [
+                DropdownMenuItem(value: 0.8, child: Text("Pequeño")),
+                DropdownMenuItem(value: 1.0, child: Text("Mediano")),
+                DropdownMenuItem(value: 1.3, child: Text("Grande")),
+              ],
+              onChanged: (value) {
+                if (value != null) {
+                  setState(() {
+                    selectedScale = value;
+                  });
+                  FontSizeController.scale.value = value;
+                }
               },
-              child: const Text("Probar notificación"),
             ),
-            
+
             const SizedBox(height: 20),
 
             ElevatedButton(
               onPressed: () {
-                FontSizeController.scale.value = 0.8; // pequeño
+                NotiService.notificacionRandom();
               },
-              child: const Text("Pequeño"),
-            ),
-
-            ElevatedButton(
-              onPressed: () {
-                FontSizeController.scale.value = 1.0; // mediano
-              },
-              child: const Text("Mediano"),
-            ),
-
-            ElevatedButton(
-              onPressed: () {
-                FontSizeController.scale.value = 1.3; // grande
-              },
-              child: const Text("Grande"),
+              child: const Text("Probar notificación"),
             ),
           ],
         ),
